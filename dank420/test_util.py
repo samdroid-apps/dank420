ce25248002f47571c533efb62f72ac88a353812e
Sam Parkinson <sam@sam.today>
2018-01-31 23:06:11 +0000

    Add util to create URL slugs in grow.io style

--- dank420/test_util.py
+++ dank420/test_util.py
@@ -7,3 +7,24 @@ def test_normalize_path():
     assert util.normalize_path('/bro/mate') == '/bro/mate'
     assert util.normalize_path('/bro/mate/') == '/bro/mate'
     assert util.normalize_path('/bro/mate//') == '/bro/mate'
+
+def test_slugify_grow():
+    # Test with a series of examples from my blog, which was previously
+    # using grow.io
+    cases = [
+        ('Gtk+ 3.22 theme support',
+            'gtk+-3-22-theme-support'),
+        ('A Grow.io Introduction',
+            'a-grow-io-introduction'),
+        ('Liberating Presenter Club',
+            'liberating-presenter-club'),
+        ('Environments with Nix Shell - Learning Nix pt 1',
+            'environments-with-nix-shell-learning-nix-pt-1'),
+        ('How they track you: Email Service Provider Edition',
+            'how-they-track-you:-email-service-provider-edition'),
+        ('6 Stunning Email SignUp Form Designs with Free HTML',
+            '6-stunning-email-signup-form-designs-with-free-html'),
+        ('My WATCH runs GNU/Linux And It Is Amazing',
+            'my-watch-runs-gnu-linux-and-it-is-amazing')]
+    for value, expected in cases:
+        assert util.slugify_grow(value) == expected
--- dank420/util.py
+++ dank420/util.py
@@ -1,3 +1,4 @@
+import re
 
 def normalize_path(path):
     '''
@@ -7,4 +8,25 @@ def normalize_path(path):
         return '/'
     return path.rstrip('/')
 
+
+_GROW_SLUG_REGEX = re.compile(r'[\t !"#$%&\'()*\-/<=>?@\[\\\]^_`{|},.]+')
+
+def slugify_grow(text, delim=u'-'):
+    '''
+    A function to convert some text into a URL slug (a string of characters
+    that is URL safe)
+
+    It is very similar to the one included in grow.io, although does have
+    the same behaviour for non-ascii characters.  This is still useful
+    if you are converting your site.
+
+    Args:
+        text (string): text to slugify
+        delim (string): separator between words for slug output
+
+    Returns:
+        string, a url safe string, containing no slashes
+    '''
+    words = [word for word in _GROW_SLUG_REGEX.split(text.lower()) if word]
+    return delim.join(words)
 
